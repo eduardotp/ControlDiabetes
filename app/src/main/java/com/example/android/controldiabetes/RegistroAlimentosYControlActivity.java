34983551e4147677962de72a8da8b671667e12be
ANDROID <torresluis082@gmail.com>
2016-03-19 15:08:44 -0500

    ff

---
+++ app/src/main/java/com/example/android/controldiabetes/RegistroAlimentosYControlActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.controldiabetes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistroAlimentosYControlActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_alimentos_ycontrol);
+    }
+
+
+
+}
