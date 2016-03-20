064de5f1c0a28400ae4fea60bc3235edaf7aa1c7
ANDROID <torresluis082@gmail.com>
2016-03-19 19:38:33 -0500

    yyyy

--- app/src/main/java/com/example/android/controldiabetes/TendenciaHistoricaActivity.java
+++ app/src/main/java/com/example/android/controldiabetes/TendenciaHistoricaActivity.java
@@ -3,13 +3,11 @@ package com.example.android.controldiabetes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class RegistroAlimentosYControlActivity extends AppCompatActivity {
+public class TendenciaHistoricaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registro_alimentos_ycontrol);
+        setContentView(R.layout.activity_tendencia_historica);
     }
-
-
 }
