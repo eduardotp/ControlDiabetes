4feb030622096e35e524231736dadc5a8aa73bc1
tsla082 <torresluis082@gmail.com>
2016-04-02 18:49:04 -0500

    icon launcher updated and changes image for all resolutions :V

--- app/src/main/java/com/example/android/controldiabetes/RegistroAlimentosYControlActivity.java
+++ app/src/main/java/com/example/android/controldiabetes/RegistroAlimentosYControlActivity.java
@@ -1,14 +1,21 @@
 package com.example.android.controldiabetes;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class RegistroAlimentosYControlActivity extends AppCompatActivity {
+public class RegistroAlimentosYControlActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_registro_alimentos_ycontrol);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 
 
