77b8e8960719b34c03d3e4f89bf1968738cab5eb
tsla082 <torresluis082@gmail.com>
2016-04-02 01:02:25 -0500

    icon launcher updated and changes image for all resolutions :V

---
+++ app/src/main/java/com/example/android/controldiabetes/Splash.java
@@ -0,0 +1,48 @@
+package com.example.android.controldiabetes;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+/**
+ * Created by Luis on 06/12/2015.
+ */
+public class Splash extends Activity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.splash);
+
+        final ImageView imageView=(ImageView)findViewById(R.id.imageView);
+        final Animation an= AnimationUtils.loadAnimation(getBaseContext(), R.anim.rotate);
+
+        imageView.startAnimation(an);
+
+        an.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                finish();
+                Intent intent=new Intent(getBaseContext(),RegistroAlimentosYControlActivity.class);
+                startActivity(intent);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+    }
+}
